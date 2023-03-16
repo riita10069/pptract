c33c4d39e36bc48c3955f1bffa1c57e9b5095c47
Ryota Yamada <riita@amazon.co.jp>
2023-03-17 01:37:34 +0900

    feat: 🎸 delete unnecessary \n

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ def extract_text(filepath):
         raise click.FileError(filepath, "File does not exist.")
 
     prs = Presentation(filepath)
-    text_runs = []
+    text_runs_arr = []
 
     for slide in prs.slides:
         for shape in slide.shapes:
             if not shape.has_text_frame:
                 continue
             for paragraph in shape.text_frame.paragraphs:
+                text_runs = ""
                 for run in paragraph.runs:
-                    text_runs.append(run.text)
+                    text_runs += run.text
+                text_runs_arr.append(text_runs)
+            
 
-    click.echo('\n'.join(text_runs))
+    click.echo('\n'.join(text_runs_arr))
 
 if __name__ == '__main__':
     extract_text()
